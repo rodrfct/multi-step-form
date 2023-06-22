8759d3aae27aea344428003c5bc077a1467ec1a8
rodrfct <rodrigoca.fct@gmail.com>
2023-06-22 17:05:51 +0200

    Add step 4 markup and style

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -22,7 +22,8 @@ const props = defineProps({
     
         <div class="step-switcher">
             <button v-show="props.step > 1" @click="$emit('step-back')" type="button" id="back-btn">Go Back</button>
-            <button v-show="props.step <= 4" @click="$emit('step-forward')" type="button" id="forward-btn">Next Step</button>
+            <button v-show="props.step < 4" @click="$emit('step-forward')" type="button" id="forward-btn">Next Step</button>
+            <button v-show="props.step == 4" @click="$emit('confirm')" type="button" id="confirm-btn">Confirm</button>
         </div>
     </form>
 </template>
@@ -44,11 +45,13 @@ const props = defineProps({
     bottom: 5%;
 }
 
-#forward-btn {
+.step-switcher button {
     border: none;
     border-radius: 6px;
+    padding: .7em 1.5em;
+}
 
-    padding: .7em 1.3em;
+#forward-btn {
     background-color: var(--Marine-blue);
     color: var(--White);
     font-weight: 700;
@@ -64,6 +67,13 @@ const props = defineProps({
     justify-self: start;
 }
 
+#confirm-btn {
+    background-color: var(--Purplish-blue);
+    color: var(--White);
+    font-weight: 700;
+    justify-self: end;
+}
+
 /*Actual form */
 #form fieldset {
     border: none;
@@ -72,7 +82,7 @@ const props = defineProps({
 }
 
 #form legend {
-    font-size: 1.6em;
+    font-size: 1.75em;
     font-weight: 700;
 }
 
--- src/components/FormStep4.vue
+++ src/components/FormStep4.vue
@@ -1,5 +1,85 @@
 <script setup lang="ts"></script>
 
 <template>
-    <h1>Step 4</h1>
+    <fieldset>
+        <legend>Finishing up</legend>
+        <p>Double-check everything looks OK before confirming.</p>
+
+        <div class="summary">
+            <div class="item plan">
+                <div class="selection">
+                    <span>Arcade (Monthly)</span>
+                    <p>Change</p>
+                </div>
+
+                <span class="price">$9/mo</span>
+            </div>
+            <div class="item">
+                <p>Online service</p>
+                <span class="price">+$1/mo</span>
+            </div>
+            <div class="item">
+                <p>Larger storage</p>
+                <span class="price">+$2/mo</span>
+            </div>
+        </div>
+
+        <div class="item total">
+            <p>Total (per month)</p>
+
+            <span class="price">+$12/mo</span>
+        </div>
+    </fieldset>
 </template>
+
+<style scoped>
+.summary {
+    background-color: var(--Magnolia);
+    padding: 15px 0;
+    margin-top: 10%;
+}
+
+.item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    margin: 2% 6%;
+}
+
+.item p {
+    margin: 2% 0;
+}
+
+.plan {
+    border-bottom: 2px solid var(--Light-gray);
+}
+
+.plan > span {
+    font-weight: 500;
+}
+
+.plan > * {
+    margin-bottom: 7%;
+}
+
+.selection span {
+    font-weight: 500;
+}
+
+.selection p {
+    margin: 5px 0 0 0;
+    text-decoration: underline;
+    cursor: pointer;
+}
+
+.total {
+    margin-top: 5%;
+}
+
+.total span {
+    color: var(--Purplish-blue);
+    font-weight: 700;
+    font-size: 1.1em;
+}
+</style>
